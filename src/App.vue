45b3f6e2ee7f5afa45228c732876590f71f877d4
abhi0498 <randyabhi65@gmail.com>
2019-08-16 23:25:39 +0530

    Semester selector added

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,48 @@
 <template>
   <div id="app">
     <Navbar />
-    <Form />
+    <div v-if="click" class="sem m-2">
+      <form @submit.prevent="change">
+        <label for="sem">Select you semester</label>
+        <select class="custom-select my-1 mr-sm-2" id="sem" v-model.number="sem">
+          <option value="1">One</option>
+          <option value="2">Two</option>
+          <option value="3">Three</option>
+          <option value="3">Four</option>
+          <option value="3">Five</option>
+          <option value="3">Six</option>
+          <option value="3">Seven</option>
+        </select>
+        <center>
+          <button class="m-2" v-if="sem!=null" @click="clicked">Submit</button>
+        </center>
+      </form>
+    </div>
+    <Form v-if="!click" />
   </div>
 </template>
 
 <script>
 import Navbar from "./components/Navbar";
 import Form from "./components/Form";
+
 export default {
   name: "app",
   components: {
     Navbar,
     Form
+  },
+  data() {
+    return {
+      sem: null,
+      click: true
+    };
+  },
+  methods: {
+    clicked: function() {
+      this.click = false;
+      console.log(this.sem);
+    }
   }
 };
 </script>
---
+++ src/components/Sem.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <form @submit.prevent="change">
+      <label for="sem">Select you semester</label>
+      <select class="custom-select my-1 mr-sm-2" id="sem" v-model.number="sem">
+        <option selected>Choose...</option>
+        <option value="1">One</option>
+        <option value="2">Two</option>
+        <option value="3">Three</option>
+        <option value="3">Four</option>
+        <option value="3">Five</option>
+        <option value="3">Six</option>
+        <option value="3">Seven</option>
+      </select>
+      <button>Submit</button>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Sem",
+  data() {
+    return {
+      sem: null
+    };
+  },
+  methods: {
+    change: function() {
+      console.log(this);
+    }
+  }
+};
+</script>
+
+<style>
+</style>
